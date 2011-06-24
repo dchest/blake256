7bff3b21f38d4fb5ff7f9c97cb7714386a2a27a1
Dmitry Chestnykh <dmitry@codingrobots.com>
2011-06-25 01:10:37 +0200

    Turn sigma from slice into array.
    Performance was 30 MB/s, now 37 MB/s. Getting close to SHA-256!

--- blake256.go
+++ blake256.go
@@ -26,7 +26,7 @@ type digest struct {
 	gotsum bool // indicates whether Sum was called
 }
 
-var sigma = [][]uint8{
+var sigma = [...][...]uint8{
 	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
 	{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
 	{11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
