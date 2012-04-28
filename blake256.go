6bb38ac596108114e0b9681b45173da23c8fa27f
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-04-28 13:55:41 +0200

    Fix typo.

--- blake256.go
+++ blake256.go
@@ -1,6 +1,6 @@
 // Written in 2011-2012 by Dmitry Chestnykh.
 //
-// To the extent possible under law, the author(s) have dedicated all copyright
+// To the extent possible under law, the author have dedicated all copyright
 // and related and neighboring rights to this software to the public domain
 // worldwide. This software is distributed without any warranty.
 // http://creativecommons.org/publicdomain/zero/1.0/
