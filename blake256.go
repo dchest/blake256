d0b32955b51e79a2f45cc09ba3294f2fbd8ac213
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-04-28 12:44:44 +0200

    Simplify updating. Make _Block compress many blocks.
    * Make _Block a function, not a method, and let it process more than one
      block.
    * Make Write similar to the one in SHA-256.
    * t is now uint64 instead of array of uint32's.
    * Simplify New* functions.

--- blake256.go
+++ blake256.go
@@ -12,13 +12,13 @@ import "hash"
 const BlockSize = 64
 
 type digest struct {
-	hashSize int              // hash output size in bits (224 or 256)
-	h        [8]uint32        // current chain value
-	salt     [4]uint32        // salt (zero by default)
-	t        [2]uint32        // counter of hashed bits
-	nullt    bool             // special case for finalization
-	buf      [BlockSize]uint8 // cache for data not yet compressed
-	buflen   int              // cache length in bits
+	hashSize int             // hash output size in bits (224 or 256)
+	h        [8]uint32       // current chain value
+	s        [4]uint32       // salt (zero by default)
+	t        uint64          // message length counter in bits
+	nullt    bool            // special case for finalization: skip counter
+	x        [BlockSize]byte // buffer for data not yet compressed
+	nx       int             // number of bytes in buffer
 }
 
 var (
@@ -56,111 +56,115 @@ var (
 		0xFFC00B31, 0x68581511, 0x64F98FA7, 0xBEFA4FA4}
 )
 
-func (d *digest) _Block(p []uint8) {
+func _Block(d *digest, p []uint8) int {
 	var m [16]uint32
-	for i := 0; i < 16; i++ {
-		j := i * 4
-		m[i] = uint32(p[j])<<24 | uint32(p[j+1])<<16 | uint32(p[j+2])<<8 | uint32(p[j+3])
-	}
-	v0 := d.h[0]
-	v1 := d.h[1]
-	v2 := d.h[2]
-	v3 := d.h[3]
-	v4 := d.h[4]
-	v5 := d.h[5]
-	v6 := d.h[6]
-	v7 := d.h[7]
-	v8 := d.salt[0] ^ cst[0]
-	v9 := d.salt[1] ^ cst[1]
-	v10 := d.salt[2] ^ cst[2]
-	v11 := d.salt[3] ^ cst[3]
-	v12 := cst[4]
-	v13 := cst[5]
-	v14 := cst[6]
-	v15 := cst[7]
-	if !d.nullt {
-		v12 ^= d.t[0]
-		v13 ^= d.t[0]
-		v14 ^= d.t[1]
-		v15 ^= d.t[1]
-	}
+	n := 0
+	h0, h1, h2, h3, h4, h5, h6, h7 := d.h[0], d.h[1], d.h[2], d.h[3], d.h[4], d.h[5], d.h[6], d.h[7]
 
-	for i := 0; i < 14; i++ {
-		si := &sigma[i]
-		v0 += (m[si[0]] ^ cst[si[0+1]]) + v4
-		v12 = (v12^v0)<<(32-16) | (v12^v0)>>16
-		v8 += v12
-		v4 = (v4^v8)<<(32-12) | (v4^v8)>>12
-		v0 += (m[si[0+1]] ^ cst[si[0]]) + v4
-		v12 = (v12^v0)<<(32-8) | (v12^v0)>>8
-		v8 += v12
-		v4 = (v4^v8)<<(32-7) | (v4^v8)>>7
-		v1 += (m[si[2]] ^ cst[si[2+1]]) + v5
-		v13 = (v13^v1)<<(32-16) | (v13^v1)>>16
-		v9 += v13
-		v5 = (v5^v9)<<(32-12) | (v5^v9)>>12
-		v1 += (m[si[2+1]] ^ cst[si[2]]) + v5
-		v13 = (v13^v1)<<(32-8) | (v13^v1)>>8
-		v9 += v13
-		v5 = (v5^v9)<<(32-7) | (v5^v9)>>7
-		v2 += (m[si[4]] ^ cst[si[4+1]]) + v6
-		v14 = (v14^v2)<<(32-16) | (v14^v2)>>16
-		v10 += v14
-		v6 = (v6^v10)<<(32-12) | (v6^v10)>>12
-		v2 += (m[si[4+1]] ^ cst[si[4]]) + v6
-		v14 = (v14^v2)<<(32-8) | (v14^v2)>>8
-		v10 += v14
-		v6 = (v6^v10)<<(32-7) | (v6^v10)>>7
-		v3 += (m[si[6]] ^ cst[si[6+1]]) + v7
-		v15 = (v15^v3)<<(32-16) | (v15^v3)>>16
-		v11 += v15
-		v7 = (v7^v11)<<(32-12) | (v7^v11)>>12
-		v3 += (m[si[6+1]] ^ cst[si[6]]) + v7
-		v15 = (v15^v3)<<(32-8) | (v15^v3)>>8
-		v11 += v15
-		v7 = (v7^v11)<<(32-7) | (v7^v11)>>7
-		v3 += (m[si[14]] ^ cst[si[14+1]]) + v4
-		v14 = (v14^v3)<<(32-16) | (v14^v3)>>16
-		v9 += v14
-		v4 = (v4^v9)<<(32-12) | (v4^v9)>>12
-		v3 += (m[si[14+1]] ^ cst[si[14]]) + v4
-		v14 = (v14^v3)<<(32-8) | (v14^v3)>>8
-		v9 += v14
-		v4 = (v4^v9)<<(32-7) | (v4^v9)>>7
-		v2 += (m[si[12]] ^ cst[si[12+1]]) + v7
-		v13 = (v13^v2)<<(32-16) | (v13^v2)>>16
-		v8 += v13
-		v7 = (v7^v8)<<(32-12) | (v7^v8)>>12
-		v2 += (m[si[12+1]] ^ cst[si[12]]) + v7
-		v13 = (v13^v2)<<(32-8) | (v13^v2)>>8
-		v8 += v13
-		v7 = (v7^v8)<<(32-7) | (v7^v8)>>7
-		v0 += (m[si[8]] ^ cst[si[8+1]]) + v5
-		v15 = (v15^v0)<<(32-16) | (v15^v0)>>16
-		v10 += v15
-		v5 = (v5^v10)<<(32-12) | (v5^v10)>>12
-		v0 += (m[si[8+1]] ^ cst[si[8]]) + v5
-		v15 = (v15^v0)<<(32-8) | (v15^v0)>>8
-		v10 += v15
-		v5 = (v5^v10)<<(32-7) | (v5^v10)>>7
-		v1 += (m[si[10]] ^ cst[si[10+1]]) + v6
-		v12 = (v12^v1)<<(32-16) | (v12^v1)>>16
-		v11 += v12
-		v6 = (v6^v11)<<(32-12) | (v6^v11)>>12
-		v1 += (m[si[10+1]] ^ cst[si[10]]) + v6
-		v12 = (v12^v1)<<(32-8) | (v12^v1)>>8
-		v11 += v12
-		v6 = (v6^v11)<<(32-7) | (v6^v11)>>7
-	}
+	for len(p) >= BlockSize {
+		v0, v1, v2, v3, v4, v5, v6, v7 := h0, h1, h2, h3, h4, h5, h6, h7
+		v8 := cst[0] ^ d.s[0]
+		v9 := cst[1] ^ d.s[1]
+		v10 := cst[2] ^ d.s[2]
+		v11 := cst[3] ^ d.s[3]
+		v12 := cst[4]
+		v13 := cst[5]
+		v14 := cst[6]
+		v15 := cst[7]
+		d.t += 512
+		if !d.nullt {
+			v12 ^= uint32(d.t)
+			v13 ^= uint32(d.t)
+			v14 ^= uint32(d.t >> 32)
+			v15 ^= uint32(d.t >> 32)
+		}
 
-	d.h[0] ^= v0 ^ v8 ^ d.salt[0]
-	d.h[1] ^= v1 ^ v9 ^ d.salt[1]
-	d.h[2] ^= v2 ^ v10 ^ d.salt[2]
-	d.h[3] ^= v3 ^ v11 ^ d.salt[3]
-	d.h[4] ^= v4 ^ v12 ^ d.salt[0]
-	d.h[5] ^= v5 ^ v13 ^ d.salt[1]
-	d.h[6] ^= v6 ^ v14 ^ d.salt[2]
-	d.h[7] ^= v7 ^ v15 ^ d.salt[3]
+		for i := 0; i < 16; i++ {
+			j := i * 4
+			m[i] = uint32(p[j])<<24 | uint32(p[j+1])<<16 | uint32(p[j+2])<<8 | uint32(p[j+3])
+		}
+
+		for i := 0; i < 14; i++ {
+			si := &sigma[i]
+			v0 += (m[si[0]] ^ cst[si[0+1]]) + v4
+			v12 = (v12^v0)<<(32-16) | (v12^v0)>>16
+			v8 += v12
+			v4 = (v4^v8)<<(32-12) | (v4^v8)>>12
+			v0 += (m[si[0+1]] ^ cst[si[0]]) + v4
+			v12 = (v12^v0)<<(32-8) | (v12^v0)>>8
+			v8 += v12
+			v4 = (v4^v8)<<(32-7) | (v4^v8)>>7
+			v1 += (m[si[2]] ^ cst[si[2+1]]) + v5
+			v13 = (v13^v1)<<(32-16) | (v13^v1)>>16
+			v9 += v13
+			v5 = (v5^v9)<<(32-12) | (v5^v9)>>12
+			v1 += (m[si[2+1]] ^ cst[si[2]]) + v5
+			v13 = (v13^v1)<<(32-8) | (v13^v1)>>8
+			v9 += v13
+			v5 = (v5^v9)<<(32-7) | (v5^v9)>>7
+			v2 += (m[si[4]] ^ cst[si[4+1]]) + v6
+			v14 = (v14^v2)<<(32-16) | (v14^v2)>>16
+			v10 += v14
+			v6 = (v6^v10)<<(32-12) | (v6^v10)>>12
+			v2 += (m[si[4+1]] ^ cst[si[4]]) + v6
+			v14 = (v14^v2)<<(32-8) | (v14^v2)>>8
+			v10 += v14
+			v6 = (v6^v10)<<(32-7) | (v6^v10)>>7
+			v3 += (m[si[6]] ^ cst[si[6+1]]) + v7
+			v15 = (v15^v3)<<(32-16) | (v15^v3)>>16
+			v11 += v15
+			v7 = (v7^v11)<<(32-12) | (v7^v11)>>12
+			v3 += (m[si[6+1]] ^ cst[si[6]]) + v7
+			v15 = (v15^v3)<<(32-8) | (v15^v3)>>8
+			v11 += v15
+			v7 = (v7^v11)<<(32-7) | (v7^v11)>>7
+			v3 += (m[si[14]] ^ cst[si[14+1]]) + v4
+			v14 = (v14^v3)<<(32-16) | (v14^v3)>>16
+			v9 += v14
+			v4 = (v4^v9)<<(32-12) | (v4^v9)>>12
+			v3 += (m[si[14+1]] ^ cst[si[14]]) + v4
+			v14 = (v14^v3)<<(32-8) | (v14^v3)>>8
+			v9 += v14
+			v4 = (v4^v9)<<(32-7) | (v4^v9)>>7
+			v2 += (m[si[12]] ^ cst[si[12+1]]) + v7
+			v13 = (v13^v2)<<(32-16) | (v13^v2)>>16
+			v8 += v13
+			v7 = (v7^v8)<<(32-12) | (v7^v8)>>12
+			v2 += (m[si[12+1]] ^ cst[si[12]]) + v7
+			v13 = (v13^v2)<<(32-8) | (v13^v2)>>8
+			v8 += v13
+			v7 = (v7^v8)<<(32-7) | (v7^v8)>>7
+			v0 += (m[si[8]] ^ cst[si[8+1]]) + v5
+			v15 = (v15^v0)<<(32-16) | (v15^v0)>>16
+			v10 += v15
+			v5 = (v5^v10)<<(32-12) | (v5^v10)>>12
+			v0 += (m[si[8+1]] ^ cst[si[8]]) + v5
+			v15 = (v15^v0)<<(32-8) | (v15^v0)>>8
+			v10 += v15
+			v5 = (v5^v10)<<(32-7) | (v5^v10)>>7
+			v1 += (m[si[10]] ^ cst[si[10+1]]) + v6
+			v12 = (v12^v1)<<(32-16) | (v12^v1)>>16
+			v11 += v12
+			v6 = (v6^v11)<<(32-12) | (v6^v11)>>12
+			v1 += (m[si[10+1]] ^ cst[si[10]]) + v6
+			v12 = (v12^v1)<<(32-8) | (v12^v1)>>8
+			v11 += v12
+			v6 = (v6^v11)<<(32-7) | (v6^v11)>>7
+		}
+		h0 ^= v0 ^ v8 ^ d.s[0]
+		h1 ^= v1 ^ v9 ^ d.s[1]
+		h2 ^= v2 ^ v10 ^ d.s[2]
+		h3 ^= v3 ^ v11 ^ d.s[3]
+		h4 ^= v4 ^ v12 ^ d.s[0]
+		h5 ^= v5 ^ v13 ^ d.s[1]
+		h6 ^= v6 ^ v14 ^ d.s[2]
+		h7 ^= v7 ^ v15 ^ d.s[3]
+
+		p = p[BlockSize:]
+		n += BlockSize
+	}
+	d.h[0], d.h[1], d.h[2], d.h[3], d.h[4], d.h[5], d.h[6], d.h[7] = h0, h1, h2, h3, h4, h5, h6, h7
+	return n
 }
 
 // Reset resets the state of digest. It leaves salt intact.
@@ -170,63 +174,35 @@ func (d *digest) Reset() {
 	} else {
 		d.h = iv256
 	}
-	d.t[0] = 0
-	d.t[1] = 0
+	d.t = 0
+	d.nx = 0
 	d.nullt = false
-	d.buflen = 0
 }
 
 func (d *digest) Size() int { return d.hashSize >> 3 }
 
 func (d *digest) BlockSize() int { return BlockSize }
 
-// update updates the internal state of digest with the given data,
-// of the given length in bits (!).
-func (d *digest) update(data []byte, datalen uint64) {
-	left := d.buflen >> 3
-	fill := 64 - left
-
-	if left != 0 && int(datalen>>3)&0x3F >= fill {
-		copy(d.buf[left:], data[:fill])
-		d.t[0] += 512
-		if d.t[0] == 0 {
-			d.t[1]++
-		}
-		d._Block(d.buf[:])
-		data = data[fill:]
-		datalen -= uint64(fill) << 3
-		left = 0
-	}
-
-	for datalen >= 512 {
-		d.t[0] += 512
-		if d.t[0] == 0 {
-			d.t[1]++
-		}
-		d._Block(data[:64])
-		data = data[64:]
-		datalen -= 512
-	}
-
-	if datalen > 0 {
-		copy(d.buf[left:], data)
-		d.buflen = left<<3 + int(datalen)
-	} else {
-		d.buflen = 0
-	}
-}
-
 func (d *digest) Write(p []byte) (nn int, err error) {
 	nn = len(p)
-	d.update(p, uint64(nn)*8)
+	if d.nx > 0 {
+		n := len(p)
+		if n > BlockSize-d.nx {
+			n = BlockSize - d.nx
+		}
+		d.nx += copy(d.x[d.nx:], p)
+		if d.nx == BlockSize {
+			_Block(d, d.x[:])
+			d.nx = 0
+		}
+		p = p[n:]
+	}
+	n := _Block(d, p)
+	p = p[n:]
+	if len(p) > 0 {
+		d.nx = copy(d.x[:], p)
+	}
 	return
-}
-
-func u32to8(p []byte, v uint32) {
-	p[0] = byte(v >> 24)
-	p[1] = byte(v >> 16)
-	p[2] = byte(v >> 8)
-	p[3] = byte(v)
 }
 
 // Sum returns the calculated checksum.
@@ -234,47 +210,52 @@ func (d0 *digest) Sum(in []byte) []byte {
 	// Make a copy of d0 so that caller can keep writing and summing.
 	d := *d0
 
-	ubuflen := uint32(d.buflen)
-	lo := d.t[0] + ubuflen
-	hi := d.t[1]
-	if lo < ubuflen {
-		hi++
-	}
-	msglen := make([]byte, 8)
-	u32to8(msglen[0:4], hi)
-	u32to8(msglen[4:8], lo)
+	nx := uint64(d.nx)
+	l := d.t + nx<<3
+	len := make([]byte, 8)
+	len[0] = byte(l >> 56)
+	len[1] = byte(l >> 48)
+	len[2] = byte(l >> 40)
+	len[3] = byte(l >> 32)
+	len[4] = byte(l >> 24)
+	len[5] = byte(l >> 16)
+	len[6] = byte(l >> 8)
+	len[7] = byte(l)
 
-	if d.buflen == 440 { // one padding byte
-		d.t[0] -= 8
+	if nx == 55 {
+		// One padding byte.
+		d.t -= 8
 		if d.hashSize == 224 {
-			d.update([]byte{0x80}, 8)
+			d.Write([]byte{0x80})
 		} else {
-			d.update([]byte{0x81}, 8)
+			d.Write([]byte{0x81})
 		}
 	} else {
 		pad := [64]byte{0x80}
-		if d.buflen < 440 { // enough space to fill the block
-			if d.buflen == 0 {
+		if nx < 55 {
+			// Enough space to fill the block.
+			if nx == 0 {
 				d.nullt = true
 			}
-			d.t[0] -= 440 - ubuflen
-			d.update(pad[:], uint64(440-d.buflen))
-		} else { // need 2 compressions
-			d.t[0] -= 512 - ubuflen
-			d.update(pad[:], uint64(512-d.buflen))
-			d.t[0] -= 440
-			d.update(pad[1:], 440)
+			d.t -= 440 - nx<<3
+			d.Write(pad[0 : 55-nx])
+		} else {
+			// Need 2 compressions.
+			d.t -= 512 - nx<<3
+			d.Write(pad[0 : 64-nx])
+			d.t -= 440
+			d.Write(pad[1:56])
 			d.nullt = true
 		}
 		if d.hashSize == 224 {
-			d.update([]byte{0x00}, 8)
+			d.Write([]byte{0x00})
 		} else {
-			d.update([]byte{0x01}, 8)
+			d.Write([]byte{0x01})
 		}
-		d.t[0] -= 8
+		d.t -= 8
 	}
-	d.t[0] -= 64
-	d.update(msglen, 64)
+	d.t -= 64
+	d.Write(len)
 
 	out := make([]byte, d.Size())
 	j := 0
@@ -292,39 +273,44 @@ func (d *digest) setSalt(s []byte) {
 	if len(s) != 16 {
 		panic("salt length must be 16 bytes")
 	}
-	d.salt[0] = uint32(s[0])<<24 | uint32(s[1])<<16 | uint32(s[2])<<8 | uint32(s[3])
-	d.salt[1] = uint32(s[4])<<24 | uint32(s[5])<<16 | uint32(s[6])<<8 | uint32(s[7])
-	d.salt[2] = uint32(s[8])<<24 | uint32(s[9])<<16 | uint32(s[10])<<8 | uint32(s[11])
-	d.salt[3] = uint32(s[12])<<24 | uint32(s[13])<<16 | uint32(s[14])<<8 | uint32(s[15])
-}
-
-func newDigest(bitSize int) (d *digest) {
-	d = new(digest)
-	d.hashSize = bitSize
-	d.Reset()
-	return
+	d.s[0] = uint32(s[0])<<24 | uint32(s[1])<<16 | uint32(s[2])<<8 | uint32(s[3])
+	d.s[1] = uint32(s[4])<<24 | uint32(s[5])<<16 | uint32(s[6])<<8 | uint32(s[7])
+	d.s[2] = uint32(s[8])<<24 | uint32(s[9])<<16 | uint32(s[10])<<8 | uint32(s[11])
+	d.s[3] = uint32(s[12])<<24 | uint32(s[13])<<16 | uint32(s[14])<<8 | uint32(s[15])
 }
 
 // New returns a new hash.Hash computing the BLAKE-256 checksum.
 func New() hash.Hash {
-	return newDigest(256)
-}
-
-// New224 returns a new hash.Hash computing the BLAKE-224 checksum.
-func New224() hash.Hash {
-	return newDigest(224)
+	return &digest{
+		hashSize: 256,
+		h: iv256,
+	}
 }
 
 // NewSalt is like New but initializes salt with the given 16-byte slice.
 func NewSalt(salt []byte) hash.Hash {
-	d := newDigest(256)
+	d := &digest{
+		hashSize: 256,
+		h: iv256,
+	}
 	d.setSalt(salt)
 	return d
 }
 
+// New224 returns a new hash.Hash computing the BLAKE-224 checksum.
+func New224() hash.Hash {
+	return &digest{
+		hashSize: 224,
+		h: iv224,
+	}
+}
+
 // New224Salt is like New224 but initializes salt with the given 16-byte slice.
 func New224Salt(salt []byte) hash.Hash {
-	d := newDigest(224)
+	d := &digest{
+		hashSize: 224,
+		h: iv224,
+	}
 	d.setSalt(salt)
 	return d
 }
