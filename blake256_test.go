3620be2d14905a165b6bf1860b82299859db7d96
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-05-25 02:35:42 +0200

    Gofmt.

--- blake256_test.go
+++ blake256_test.go
@@ -144,25 +144,25 @@ var bench = New()
 var buf = makeBuf()
 
 func makeBuf() []byte {
-        b := make([]byte, 8<<10)
-        for i := range b {
-                b[i] = byte(i)
-        }
-        return b
+	b := make([]byte, 8<<10)
+	for i := range b {
+		b[i] = byte(i)
+	}
+	return b
 }
 
 func BenchmarkHash1K(b *testing.B) {
-        b.SetBytes(1024)
-        for i := 0; i < b.N; i++ {
-                bench.Write(buf[:1024])
-        }
+	b.SetBytes(1024)
+	for i := 0; i < b.N; i++ {
+		bench.Write(buf[:1024])
+	}
 }
 
 func BenchmarkHash8K(b *testing.B) {
-        b.SetBytes(int64(len(buf)))
-        for i := 0; i < b.N; i++ {
-                bench.Write(buf)
-        }
+	b.SetBytes(int64(len(buf)))
+	for i := 0; i < b.N; i++ {
+		bench.Write(buf)
+	}
 }
 
 func BenchmarkFull64(b *testing.B) {
